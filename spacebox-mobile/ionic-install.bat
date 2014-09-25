68246cf3a32df581f0181736bbe69ad13d36d96f
artemgalin <agalin89@yandex.ru>
2014-09-26 00:55:28 +0400

    Update

---
+++ spacebox-mobile/ionic-install.bat
@@ -0,0 +1,2 @@
+call ionic platform add android
+call ionic plugin add com.ionic.keyboard org.apache.cordova.console org.apache.cordova.device org.apache.cordova.inappbrowser org.apache.cordova.geolocation
