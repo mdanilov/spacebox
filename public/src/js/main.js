eeab65db0168e8dfa1aa46d437ea66158afb2e48
Maxim Danilov <mdanilov@me.com>
2014-10-09 00:37:38 +0400

    Navbar is ready.

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -2,31 +2,24 @@
     test: config.development,
     yep: [
         './src/css/main.css',
-        './src/css/navbar.css',
+        './src/css/navigation.css',
         './src/css/login.css',
         './src/css/modal.css',
         './src/css/user-list.css'
     ],
     nope: './dist/spacebox.min.css',
     both: [
-        './lib/mapbox.js/mapbox.css',
-        './lib/bootstrap-material-design/dist/css/material.min.css',
-        './lib/bootstrap-material-design/dist/css/material-wfont.min.css',
-        './lib/bootstrap-material-design/dist/css/ripples.min.css'
+        './lib/mapbox.js/mapbox.css'
     ]
 });
 
 yepnope([
     'https://vk.com/js/api/openapi.js',
     './lib/mapbox.js/mapbox.js',
-    './lib/jquery/dist/jquery.min.js',
-    './lib/bootstrap/dist/js/bootstrap.min.js',
     './lib/angular/angular.js',
     './lib/angular-route/angular-route.min.js',
     './lib/angular-animate/angular-animate.js',
     './lib/angular-bootstrap/ui-bootstrap-tpls.js',
-    './lib/bootstrap-material-design/dist/js/material.min.js',
-    './lib/bootstrap-material-design/dist/js/ripples.min.js',
     './lib/jcarousel/dist/jquery.jcarousel.min.js'
 ]);
 
