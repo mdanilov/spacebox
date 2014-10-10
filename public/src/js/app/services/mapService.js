f7728be95144cb9dc0c76ebafaaf73d16b2fc0dc
Maxim Danilov <mdanilov@me.com>
2014-10-11 02:10:44 +0400

    Added tooltip window.

--- public/src/js/app/services/mapService.js
+++ public/src/js/app/services/mapService.js
@@ -5,49 +5,69 @@ function MapService ($log, $filter, GeolocationService) {
     MapService._markers = {};
     MapService._map = {};
     MapService._locator = {};
+    MapService._popupFixed = false;
     MapService.MAPBOX = {
         ACCESS_TOKEN: 'pk.eyJ1IjoibWRhbmlsb3YiLCJhIjoiV29JVmpxdyJ9.sBimZ4oSZYSFTdcZIgnQfQ',
         URL: 'mdanilov.j8f4ggll'
     };
 
+    MapService._icons = {
+        default:  L.divIcon({
+            className: 'info',
+            html: '<span class="icon icon-material-accessibility icon-material-indigo sp-user-marker"></span>',
+            iconSize: [30, 30]
+        }),
+        selected: L.divIcon({
+            className: 'info',
+            html: '<span class="icon icon-material-accessibility icon-material-indigo sp-user-marker-selected"></span>',
+            iconSize: [30, 30]
+        }),
+        locator: L.divIcon({
+            className: 'info',
+            html: '<span class="icon icon-material-place icon-material-red sp-locator-marker"></span>',
+            iconSize: [40, 40]
+        })
+    };
+
     function bindPopupWindow (marker, user) {
-        marker.bindPopup([
-            '<div style="overflow:hidden;width:200px;">',
-                '<div style="float:left;">',
-                    '<img src="' + user.largePhotoUrl + '" alt="icon">',
-                '</div>',
-                '<div style="float:left;padding:5px;">',
-                    '<h4>' + user.firstName + '</h4>',
-                    '<p>' + $filter('age')(user.bdate) + '</p>',
-                    '<p>' + $filter('distance')(user.distance) + '</p>',
-                '</div>',
-            '</div>'
-        ].join(''));
+        var popup = L.popup({
+                closeButton: false
+            }).setContent([
+                '<div class="sp-map-tooltip">',
+                    '<span class="sp-map-tooltip-carat"></span>',
+                    '<div style="width:250px">',
+                        '<img class="circle" src="' + user.largePhotoUrl + '">',
+                        '<div>',
+                            '<h4>' + user.firstName + '</h4>',
+                            '<p>' + $filter('age')(user.bdate) + '</p>',
+                            '<p>' + $filter('distance')(user.distance) + '</p>',
+                        '</div>',
+                    '</div>',
+                '</div>'
+            ].join('')
+        );
 
-        marker.on('mouseover', marker.openPopup);
-        marker.on('mouseout', marker.closePopup);
+        marker.bindPopup(popup);
 
+        marker.on('mouseover', function () {
+            marker.openPopup();
+        });
+        marker.on('mouseout', function () {
+            if (!MapService._popupFixed) {
+                marker.closePopup();
+            }
+        });
         marker.on('click', function () {
             marker.openPopup();
-            marker.off('mouseout');
-            marker.isClicked = true;
-        });
-        marker.on('popupclose', function () {
-            if (marker.isClicked) {
-                marker.on('mouseout', marker.closePopup);
-                marker.isClicked = false;
-            }
+            MapService._popupFixed = true;
         });
     }
 
     function addMarker (user) {
-        var icon = L.divIcon({
-            className: 'info',
-            html: '<span class="icon icon-material-accessibility icon-material-indigo user-marker"></span>',
-            iconSize: [30, 30]
+        var marker = L.marker([user.location.latitude, user.location.longitude], {
+            icon: MapService._icons.default,
+            riseOnHover: true
         });
-
-        var marker = L.marker([user.location.latitude, user.location.longitude], {icon: icon});
         marker.addTo(MapService._map);
 
         MapService._markers[user.uid] = marker;
@@ -56,17 +76,11 @@ function MapService ($log, $filter, GeolocationService) {
     }
 
     function initializeLocator () {
-        var icon = L.divIcon({
-            className: 'info',
-            html: '<span class="icon icon-material-place icon-material-red locator-marker"></span>',
-            iconSize: [40, 40]
-        });
-
         MapService._locator = L.control.locate({
             drawCircle: false,
             follow: false,
             setView: true,
-            markerClass: L.marker.bind(L.marker, L.latLng(0, 0), {icon: icon}),
+            markerClass: L.marker.bind(L.marker, L.latLng(0, 0), {icon: MapService._icons.locator}),
             remainActive: true,
             keepCurrentZoomLevel: true,
             icon: 'icon icon-material-location map-icon-location',
@@ -89,6 +103,11 @@ function MapService ($log, $filter, GeolocationService) {
             MapService._map.setView(pos, 15);
         });
         MapService._map.invalidateSize();
+
+        // TODO: popupclose should be listening on marker
+        MapService._map.on('popupclose', function () {
+            MapService._popupFixed = false;
+        });
 
         initializeLocator();
     };
