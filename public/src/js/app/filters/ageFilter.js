141a70562f7dd6e97125ce4f1fdfcdcf2fbaa09f
artemgalin <agalin89@yandex.ru>
2014-10-12 23:41:21 +0400

    Add new design

--- public/src/js/app/filters/ageFilter.js
+++ public/src/js/app/filters/ageFilter.js
@@ -8,14 +8,15 @@ function AgeFilter () {
         var today = new Date();
         var age = today.getYear() - bdate.getYear();
 
-        if (((age % 10) > 4) || ((age % 10) == 0)) {
-            return age + ' лет';
-        } else if ((age % 10) == 1){
-            return age + ' год';
-        }
-        else {
-            return age + ' года';
-        }
+        // if (((age % 10) > 4) || ((age % 10) == 0)) {
+        //     return age + ' лет';
+        // } else if ((age % 10) == 1){
+        //     return age + ' год';
+        // }
+        // else {
+        //     return age + ' года';
+        // }
+        return age;
     }
 }
 
