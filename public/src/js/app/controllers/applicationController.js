2300c80e90de5bd5c986ab1b2d113470a1374adc
mdanilov <mdanilov@me.com>
2014-10-26 23:43:29 +0300

    Fixed navbar info

--- public/src/js/app/controllers/applicationController.js
+++ public/src/js/app/controllers/applicationController.js
@@ -2,18 +2,15 @@ function ApplicationController ($scope, $log, VkService, ConfigService) {
     $log.debug('Initialize application controller...');
 
     var self = this;
-
     self.user = {};
-
-    var fillUserInfo = angular.bind(self.user, function (info) {
-        self.name = info[0].first_name;
-        self.image = info[0].photo_50;
-    });
 
     $scope.$watch(function () { return ConfigService.isLogin },
         function (value) {
             if (angular.equals(value, true)) {
-                VkService.asyncGetCurrentUserInfo().then(fillUserInfo);
+                VkService.asyncGetCurrentUserInfo().then(function (info) {
+                    self.user.name = info[0].first_name;
+                    self.user.image = info[0].photo_50;
+                });
             }
         });
 }
