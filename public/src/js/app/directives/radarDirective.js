bc7678ca23a009016a4b7dcd891c61ee663afd40
artemgalin <agalin89@yandex.ru>
2014-11-05 21:15:54 +0300

    Add missed files

---
+++ public/src/js/app/directives/radarDirective.js
@@ -0,0 +1,33 @@
+function radarDirective () {
+    return {
+        restrict: 'E',
+        transclude: true,
+        scope: {
+            image: '='
+        },
+        templateUrl: 'src/js/app/templates/radar.html',
+        link: function (scope, element, attrs) {
+            function addCircle() {
+                var $circle = $('<div class="circle"></div>');
+                $circle.animate({
+                    'width': '400px',
+                    'height': '400px',
+                    'margin-top': '-200px',
+                    'margin-left': '-200px',
+                    'opacity': '0'
+                }, 6000, 'swing');
+                element.find('#spRadar').append($circle);
+            
+                setTimeout(function __remove() {
+                    $circle.remove();
+                }, 6000);
+            }
+            addCircle();
+            setInterval(addCircle, 2000);
+            element.find('#spShareButton').append(VK.Share.button(false,{type: "round", text: "Рассказать друзьям"}));
+        }
+    };
+}
+
+angular.module('spacebox').directive('spRadar',
+    [radarDirective]);
