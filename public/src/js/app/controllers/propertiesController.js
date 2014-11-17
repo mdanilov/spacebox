dd6f3db27e7c2e070189d7cc23dc7a7c3ada2372
artemgalin <agalin89@yandex.ru>
2014-11-18 01:43:03 +0300

    Add status

--- public/src/js/app/controllers/propertiesController.js
+++ public/src/js/app/controllers/propertiesController.js
@@ -1,4 +1,18 @@
 function PropertiesViewController ($scope, ConfigService) {
+	$scope.status = {text: '', length: 99, maxLength: 99};
+
+	$scope.checkLength = function() {
+		var status = $scope.status;
+		if (status.text.length > status.maxLength) {
+			status.length = 0;
+			status.text = status.text.substring(0, status.maxLength);
+		} 
+		else {
+			status.length = status.maxLength - status.text.length;			
+		}
+		$scope.status = status;
+	}
+
 	var options = ConfigService.getSearchOptions();
 	var values = [options.ageInterval.bottom,options.ageInterval.top];
 	$("#spAgeSlider").slider({
