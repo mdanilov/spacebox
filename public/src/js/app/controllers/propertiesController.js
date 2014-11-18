05eae493f61172d5137edaa80221147079274456
artemgalin <agalin89@yandex.ru>
2014-11-19 02:55:29 +0300

    Fix userList and slider mouse

--- public/src/js/app/controllers/propertiesController.js
+++ public/src/js/app/controllers/propertiesController.js
@@ -1,5 +1,5 @@
 function PropertiesViewController ($scope, ConfigService) {
-	$scope.status = {text: '', length: 99, maxLength: 99};
+	$scope.status = {text: '', length: 100, maxLength: 100};
 
 	$scope.checkLength = function() {
 		var status = $scope.status;
@@ -15,28 +15,35 @@ function PropertiesViewController ($scope, ConfigService) {
 
 	var options = ConfigService.getSearchOptions();
 	var values = [options.ageInterval.bottom,options.ageInterval.top];
-	$("#spAgeSlider").slider({
+	var ageSlider = $("#spAgeSlider").slider({
 		value: values,
 		tooltip: 'hide',
 		min: 18,
 		max: 50,
 		step: 1
 	});
-	$("#spSliderValue").text(values.join('-'));
-	$("#spAgeSlider").on("slide", function(slideEvt) {
+	$("#spSliderValue").text(ageSlider.slider('getValue').join('-'));
+	
+	ageSlider.on("slide", function(slideEvt) {
 		$("#spSliderValue").text(slideEvt.value.join('-'));
 	});
+	$("#spAgeSlider").parent().click( function(clickEvt) {
+		$("#spSliderValue").text(ageSlider.slider('getValue').join('-'));
+	});
 
-	$("#spDistanceSlider").slider({
+	var distanceSlider = $("#spDistanceSlider").slider({
 		value: options.radius/1000,
 		tooltip: 'hide',
 		min: 1,
 		max: 50,
 		step: 1
 	});
-	$("#spDistanceValue").text(options.radius/1000 + ' км');
+	$("#spDistanceValue").text(distanceSlider.slider('getValue') + ' км');
 	$("#spDistanceSlider").on("slide", function(slideEvt) {
 		$("#spDistanceValue").text(slideEvt.value + ' км');
+	});
+	$("#spDistanceSlider").parent().click( function(clickEvt) {
+		$("#spDistanceValue").text(distanceSlider.slider('getValue') + ' км');
 	});
 }
 
