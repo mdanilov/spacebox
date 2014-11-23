8e5ff77c706ac7df859740ddc1753e43d061911d
mdanilov <mdanilov@me.com>
2014-11-23 17:16:04 +0300

    Cordova loader.

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -1,20 +1,17 @@
-﻿yepnope({
-    test: config.development,
-    yep: [
+﻿function bootstrapAngular() {
+    // manually bootstrap AngularJS
+    angular.bootstrap(document, ['spacebox']);
+}
+
+if (CONFIG.CORDOVA) {
+    yepnope([
+        './lib/bootstrap/dist/css/bootstrap.css',
+        './lib/components-font-awesome/css/font-awesome.css',
         './lib/mapbox.js/mapbox.uncompressed.css',
         './lib/angular-carousel/dist/angular-carousel.css',
-        './lib/seiyria-bootstrap-slider/css/bootstrap-slider.css'
-    ],
-    nope: [
-        'https://api.tiles.mapbox.com/mapbox.js/v2.1.3/mapbox.css',
-        './lib/angular-carousel/dist/angular-carousel.min.css',
-        './lib/seiyria-bootstrap-slider/dist/css/bootstrap-slider.min.css'
-    ]
-});
-
-yepnope({
-    test: config.development,
-    yep: [
+        './lib/seiyria-bootstrap-slider/css/bootstrap-slider.css',
+        './lib/jquery/dist/jquery.js',
+        './lib/bootstrap/dist/js/bootstrap.js',
         './lib/mapbox.js/mapbox.uncompressed.js',
         './lib/leaflet.locatecontrol/src/L.Control.Locate.js',
         './lib/angular/angular.js',
@@ -23,92 +20,147 @@ yepnope({
         './lib/angular-cookies/angular-cookies.js',
         './lib/angular-animate/angular-animate.js',
         './lib/angular-carousel/dist/angular-carousel.js',
-        './lib/seiyria-bootstrap-slider/js/bootstrap-slider.js'
-    ],
-    nope: [
-        'https://api.tiles.mapbox.com/mapbox.js/v2.1.3/mapbox.js',
-        './lib/leaflet.locatecontrol/dist/L.Control.Locate.min.js',
-        'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.26/angular.min.js',
-        'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.26/angular-route.min.js',
-        'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.26/angular-animate.min.js',
-        'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.26/angular-cookies.min.js',
-        'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.26/angular-touch.min.js',
-        './lib/angular-carousel/dist/angular-carousel.min.js',
-        './lib/seiyria-bootstrap-slider/dist/bootstrap-slider.min.js'
-    ],
-    both: [
-        'https://vk.com/js/api/openapi.js',
-        'https://vk.com/js/api/share.js?90'
-    ]
-});
-
-function bootstrapAngular () {
-    // manually bootstrap AngularJS
-    angular.bootstrap(document, ['spacebox']);
-}
-
-if (config.development) {
-    // css styles
-    yepnope([
-        './src/css/main.css',
-        './src/css/map.css',
-        './src/css/navbar.css',
-        './src/css/login.css',
-        './src/css/modal.css',
-        './src/css/user-list.css',
-        './src/css/friends.css',
-        './src/css/views/properties.css',
-        './src/css/views/error.css',
-        './src/css/partials/cards.css',
-        './src/css/partials/controls.css'
+        './lib/seiyria-bootstrap-slider/js/bootstrap-slider.js',
+        '//vk.com/js/api/openapi.js',
+        '//vk.com/js/api/share.js?90'
     ]);
 
-    // main app script should be loaded first
-    yepnope('./src/js/app/app.js');
-
-    // platform dependent scripts
     yepnope({
-        test: Modernizr.touch,
-        yep: './src/js/app/services/mobile/vkService.js',
-        nope: './src/js/app/services/vkService.js'
-    });
-
-    // common scripts
-    yepnope({
-        load: [
-            './src/js/app/services/geolocationService.js',
-            './src/js/app/services/configService.js',
-            './src/js/app/services/mapService.js',
-            './src/js/app/services/meetService.js',
-            './src/js/app/services/errorHandler.js',
-            './src/js/app/services/locatorService.js',
-            './src/js/app/services/userService.js',
-            './src/js/app/controllers/applicationController.js',
-            './src/js/app/controllers/propertiesController.js',
-            './src/js/app/controllers/loginController.js',
-            './src/js/app/controllers/errorController.js',
-            './src/js/app/controllers/mainController.js',
-            './src/js/app/controllers/friendsController.js',
-            './src/js/app/controllers/userListItemController.js',
-            './src/js/app/filters/distanceFilter.js',
-            './src/js/app/filters/meetFilter.js',
-            './src/js/app/filters/ageFilter.js',
-            './src/js/app/directives/navbarDirective.js',
-            './src/js/app/directives/cardsDirective.js',
-            './src/js/app/directives/modalDirective.js',
-            './src/js/app/directives/friendListDirective.js',
-            './src/js/app/directives/friendListItemDirective.js',
-            './src/js/app/directives/radarDirective.js'
+        test: CONFIG.DEVELOPMENT,
+        yep: [
+            './dist/spacebox-mobile.js',
+            './dist/spacebox.css'
+        ],
+        nope: [
+            './dist/spacebox-mobile.min.js',
+            './dist/spacebox.min.css'
         ],
         complete: bootstrapAngular
     });
 }
 else {
     yepnope({
-        test: Modernizr.touch,
-        yep: './dist/spacebox-mobile.min.js',
-        nope: './dist/spacebox.js',
-        both: './dist/spacebox.min.css',
-        complete: bootstrapAngular
+        test: CONFIG.DEVELOPMENT,
+        yep: [
+            './lib/bootstrap/dist/css/bootstrap.css',
+            './lib/components-font-awesome/css/font-awesome.css',
+            './lib/mapbox.js/mapbox.uncompressed.css',
+            './lib/angular-carousel/dist/angular-carousel.css',
+            './lib/seiyria-bootstrap-slider/css/bootstrap-slider.css'
+        ],
+        nope: [
+            '//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css',
+            '//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css',
+            '//api.tiles.mapbox.com/mapbox.js/v2.1.3/mapbox.css',
+            './lib/angular-carousel/dist/angular-carousel.min.css',
+            './lib/seiyria-bootstrap-slider/dist/css/bootstrap-slider.min.css'
+        ]
     });
+
+    yepnope({
+        test: CONFIG.DEVELOPMENT,
+        yep: [
+            './lib/jquery/dist/jquery.js',
+            './lib/bootstrap/dist/js/bootstrap.js',
+            './lib/mapbox.js/mapbox.uncompressed.js',
+            './lib/leaflet.locatecontrol/src/L.Control.Locate.js',
+            './lib/angular/angular.js',
+            './lib/angular-route/angular-route.js',
+            './lib/angular-touch/angular-touch.js',
+            './lib/angular-cookies/angular-cookies.js',
+            './lib/angular-animate/angular-animate.js',
+            './lib/angular-carousel/dist/angular-carousel.js',
+            './lib/seiyria-bootstrap-slider/js/bootstrap-slider.js'
+        ],
+        nope: [
+            '//code.jquery.com/jquery-2.1.1.min.js',
+            '//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js',
+            '//api.tiles.mapbox.com/mapbox.js/v2.1.3/mapbox.js',
+            '//ajax.googleapis.com/ajax/libs/angularjs/1.2.26/angular.min.js',
+            '//ajax.googleapis.com/ajax/libs/angularjs/1.2.26/angular-route.min.js',
+            '//ajax.googleapis.com/ajax/libs/angularjs/1.2.26/angular-animate.min.js',
+            '//ajax.googleapis.com/ajax/libs/angularjs/1.2.26/angular-cookies.min.js',
+            '//ajax.googleapis.com/ajax/libs/angularjs/1.2.26/angular-touch.min.js',
+            './lib/leaflet.locatecontrol/dist/L.Control.Locate.min.js',
+            './lib/angular-carousel/dist/angular-carousel.min.js',
+            './lib/seiyria-bootstrap-slider/dist/bootstrap-slider.min.js'
+        ],
+        both: [
+            '//vk.com/js/api/openapi.js',
+            '//vk.com/js/api/share.js?90'
+        ]
+    });
+
+    yepnope({
+        load: './lib/modernizr/modernizr.js',
+        callback: loadApplication
+    });
+
+    function loadApplication (url, result, key) {
+        if (CONFIG.DEVELOPMENT) {
+            // css styles
+            yepnope([
+                './src/css/main.css',
+                './src/css/map.css',
+                './src/css/navbar.css',
+                './src/css/login.css',
+                './src/css/modal.css',
+                './src/css/user-list.css',
+                './src/css/friends.css',
+                './src/css/views/properties.css',
+                './src/css/views/error.css',
+                './src/css/partials/cards.css',
+                './src/css/partials/controls.css'
+            ]);
+
+            // main app script should be loaded first
+            yepnope('./src/js/app/app.js');
+
+            // platform dependent scripts
+            yepnope({
+                test: Modernizr.touch,
+                yep: './src/js/app/services/mobile/vkService.js',
+                nope: './src/js/app/services/vkService.js'
+            });
+
+            // common scripts
+            yepnope({
+                load: [
+                    './src/js/app/services/geolocationService.js',
+                    './src/js/app/services/configService.js',
+                    './src/js/app/services/mapService.js',
+                    './src/js/app/services/meetService.js',
+                    './src/js/app/services/errorHandler.js',
+                    './src/js/app/services/locatorService.js',
+                    './src/js/app/services/userService.js',
+                    './src/js/app/controllers/applicationController.js',
+                    './src/js/app/controllers/propertiesController.js',
+                    './src/js/app/controllers/loginController.js',
+                    './src/js/app/controllers/errorController.js',
+                    './src/js/app/controllers/mainController.js',
+                    './src/js/app/controllers/friendsController.js',
+                    './src/js/app/controllers/userListItemController.js',
+                    './src/js/app/filters/distanceFilter.js',
+                    './src/js/app/filters/meetFilter.js',
+                    './src/js/app/filters/ageFilter.js',
+                    './src/js/app/directives/navbarDirective.js',
+                    './src/js/app/directives/cardsDirective.js',
+                    './src/js/app/directives/modalDirective.js',
+                    './src/js/app/directives/friendListDirective.js',
+                    './src/js/app/directives/friendListItemDirective.js',
+                    './src/js/app/directives/radarDirective.js'
+                ],
+                complete: bootstrapAngular
+            });
+        }
+        else {
+            yepnope({
+                test: Modernizr.touch,
+                yep: './dist/spacebox-mobile.min.js',
+                nope: './dist/spacebox.js',
+                both: './dist/spacebox.min.css',
+                complete: bootstrapAngular
+            });
+        }
+    }
 }
