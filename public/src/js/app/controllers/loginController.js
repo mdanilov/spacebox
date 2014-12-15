97028830cc4136a63ea3fdcf2ef1b2193736d3c7
mdanilov <mdanilov@me.com>
2014-12-16 02:06:38 +0300

    login page

--- public/src/js/app/controllers/loginController.js
+++ public/src/js/app/controllers/loginController.js
@@ -3,6 +3,13 @@ function LoginViewController ($scope, $log, $location, VkService, UserService, C
 
     $scope.app.isNavbarHidden = true;
 
+    $scope.images = [
+        "src/img/login-1.jpg",
+        "src/img/login-2.jpg",
+        "src/img/login-3.jpg",
+        "src/img/login-4.jpg"
+    ];
+
     $scope.Login = function () {
         $log.debug('Try login to VK...');
         VkService.asyncLogin().then(function (id) {
