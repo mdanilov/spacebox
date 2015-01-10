619e9606cdd82347dab51e97fd437a1ad8d76667
mdanilov <mdanilov@me.com>
2015-01-11 02:44:59 +0300

    Fix trackLocation middleware.

--- middleware/trackLocation.js
+++ middleware/trackLocation.js
@@ -4,7 +4,7 @@ var async = require('async');
 var db = require('pg-bricks').configure(config.get('database:connection'));
 
 module.exports =  function (request, response, next) {
-    if (!(request.session && request.cookies.location)) {
+    if (!(request.session && request.session.mid && request.cookies.location)) {
         return next();
     }
 
@@ -20,31 +20,29 @@ module.exports =  function (request, response, next) {
     }
 
     var userId = request.session.mid;
-    if (userId) {
-        db.transaction(function (client, callback) {
-            async.waterfall([
-                client.delete().from('users').where({'mid': userId}).run,
-                function (result, callback) {
-                    var info = {};
-                    if (request.cookies.user) {
-                        info = JSON.parse(unescape(request.cookies.user));
-                    }
-                    client.insert('users', {
-                        'mid': userId,
-                        'latitude': location.coords.latitude,
-                        'longitude': location.coords.longitude,
-                        'sex': info.sex || 0,
-                        'age': info.age || -1,
-                        'timestamp': new Date(location.timestamp).toUTCString()
-                    }).run(callback);
+    db.transaction(function (client, callback) {
+        async.waterfall([
+            client.delete().from('users').where({'mid': userId}).run,
+            function (result, callback) {
+                var info = {};
+                if (request.cookies.user) {
+                    info = JSON.parse(unescape(request.cookies.user));
                 }
-            ], callback);
-        }, function (error, result) {
-            if (!(error || result.error)) {
-                request.session.location = location;
+                client.insert('users', {
+                    'mid': userId,
+                    'latitude': location.coords.latitude,
+                    'longitude': location.coords.longitude,
+                    'sex': info.sex || 0,
+                    'age': info.age || -1,
+                    'timestamp': new Date(location.timestamp).toUTCString()
+                }).run(callback);
             }
+        ], callback);
+    }, function (error, result) {
+        if (!(error || result.error)) {
+            request.session.location = location;
+        }
 
-            return next();
-        });
-    }
+        return next();
+    });
 };
