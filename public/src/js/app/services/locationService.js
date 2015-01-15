83dca8a62506aad9ed4b250ec67c5a396b59ab5a
mdanilov <mdanilov@me.com>
2015-01-15 09:49:33 +0300

    Reimplement location service

--- public/src/js/app/services/locationService.js
+++ public/src/js/app/services/locationService.js
@@ -1,43 +1,62 @@
-﻿function LocationService ($http, $log, $q, $cookieStore, ConfigService) {
+﻿function LocationService ($rootScope, $q, $timeout, $log, $cookieStore) {
 
     var LocationService = {};
-
-    LocationService.asyncGetCurrentPosition = function () {
-        var deferred = $q.defer();
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function (position) {
-                $cookieStore.put('location', position);
-                deferred.resolve(position);
-            }, function () {
-                deferred.reject(new LocationError(true));
-            });
-        }
-        else {
-            deferred.reject(new LocationError(false));
-        }
-        return deferred.promise;
+    var _location = {
+        position: null,
+        promise: null,
+        resolved: false
     };
+    var _watchId = null;
 
-    LocationService.asyncGetUserPositions = function (options) {
-        return LocationService.asyncGetCurrentPosition().then(function (position) {
-            var params = {
-                latitude: position.coords.latitude,
-                longitude: position.coords.longitude,
-                options: options
-            };
-            return $http.post(ConfigService.SERVER_URL + '/users.get', params).then(
-                function __success (response) {
-                    $log.debug('Get near users ', response.data);
-                    return response.data;
-                },
-                function __error (response) {
-                    return $q.reject(new HttpError(response.status, 'get users request failed'));
-                });
+    function onLocationFound (position) {
+        _location.position = position;
+        _location.resolved = true;
+        $cookieStore.put('location', position);
+        $rootScope.$broadcast('location.found', position);
+        $log.debug('[location] New position found', position);
+    }
+
+    function onLocationError (error) {
+        $log.debug('[location] Sorry, no position available', error);
+        if (error.code == 1) {
+            throw new LocationError(true);
+        }
+    }
+
+    function asyncGetCurrentPosition () {
+        var deferred = $q.defer();
+        navigator.geolocation.getCurrentPosition(function (position) {
+            onLocationFound(position);
+            deferred.resolve(position);
+        }, function (error) {
+            onLocationError(error);
+            deferred.reject();
         });
+        return deferred.promise;
+    }
+
+    if (navigator.geolocation) {
+        $log.debug('[location] Geolocation API is available');
+        _location.promise = asyncGetCurrentPosition();
+        $timeout(function () {
+            _watchId = navigator.geolocation.watchPosition(onLocationFound, onLocationError, {
+                enableHighAccuracy: true,
+                timeout: 27000,
+                maximumAge: 30000
+            })
+        });
+    }
+    else {
+        $log.debug('[location] Geolocation API is NOT available');
+        throw new LocationError(false);
+    }
+
+    LocationService.getCurrentPosition = function () {
+        return $q.when(_location.resolved ? _location.position : _location.promise);
     };
 
     return LocationService;
 }
 
 angular.module('spacebox').factory('LocationService',
-    ['$http', '$log', '$q', '$cookieStore', 'ConfigService', LocationService]);
+    ['$rootScope', '$q', '$timeout', '$log', '$cookieStore', LocationService]);
